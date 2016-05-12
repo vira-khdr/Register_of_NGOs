52999bd10e19d8e97d7c9c894d08fc8755b645f7
Karim <karim5huder@gmail.com>
2016-05-12 11:02:08 +0300

    + party

--- register_project/project/register/views.py
+++ register_project/project/register/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 
-from .models import Commerce_Chambers
+from .models import Commerce_Chambers, Party
 import add_organization
 
 def index(request):
@@ -9,10 +9,19 @@ def index(request):
 
 def page_commerse_chambers(request):
     return render_to_response('add_commerce_chambers.html')
+def page_party(request):
+    return render_to_response('add_party.html')
 
 @csrf_exempt
 def add_commerse_chambers(request):
     if request.method == "POST":
         add_organization.add_commerce_chambers(request.POST)
+
+    return render_to_response('index.html')
+
+@csrf_exempt
+def add_party(request):
+    if request.method == "POST":
+        add_organization.add_party(request.POST)
 
     return render_to_response('index.html')
